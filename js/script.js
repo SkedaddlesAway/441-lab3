d6d877b9f2f7c4e66e8bab34d1927fce495ad408
SkedaddlesAway <abraxoszz@gmail.com>
2024-04-03 23:51:19 -0500

    Fixing final errors

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
+//active inputs
+const bill = document.getElementById('bill').value
+const tip = document.getElementById('slider').value
+
+//disabled inputs 
 let percent = document.getElementById('percent')
 let amount = document.getElementById('amount')
 let total = document.getElementById('total')
 
+// percent.value = tipValue + "%";
+// amount.value = (bill * tip / 100).toFixed(2);
+// total.value =  (bill + parseFloat(amount)).toFixed(2);
+
+//event listeners
+function calculate(){
+  if (isNaN(bill)) {
+    errorMssg.innerHTML =("Please enter a number for Bill Amount.");
+  }
+  else{
+    percent.value = tip + "%" 
+    amount.value = (bill * tip / 100).toFixed(2);
+    total.value =  (bill + parseFloat(amount)).toFixed(2);
+  }
+}
