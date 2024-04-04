35b713aa71eb2d1a18d4274ccb8c9567cc37d1be
SkedaddlesAway <abraxoszz@gmail.com>
2024-04-04 00:18:11 -0500

    ALMOST THERE: tip amount fixes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-//active inputs
-const bill = document.getElementById('bill').value
-const tip = document.getElementById('slider').value
-
-//disabled inputs 
-let percent = document.getElementById('percent')
-let amount = document.getElementById('amount')
-let total = document.getElementById('total')
 
 // percent.value = tipValue + "%";
 // amount.value = (bill * tip / 100).toFixed(2);
 // total.value =  (bill + parseFloat(amount)).toFixed(2);
 
-//event listeners
-function calculate(){
+//calculations
+calculate = () =>{
+  //active inputs
+  const bill = document.getElementById('bill').value
+  const tip = document.getElementById('slider').value
+
+  //disabled inputs 
+  let percent = document.getElementById('percent')
+  let amount = document.getElementById('amount')
+  let total = document.getElementById('total')
+
   if (isNaN(bill)) {
     errorMssg.innerHTML =("Please enter a number for Bill Amount.");
   }
   else{
     percent.value = tip + "%" 
-    amount.value = (bill * tip / 100).toFixed(2);
-    total.value =  (bill + parseFloat(amount)).toFixed(2);
+    amount.value = "$" + (bill * tip / 100).toFixed(2);
+    total.value =  "$" + (bill + parseFloat(amount)).toFixed(2);
   }
 }
+
+
+document.getElementById('calculator').addEventListener("change", calculate);
